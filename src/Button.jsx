2c0b6174554389f5df79e9873ab184bbbad31f21
Dara92 <dara92@example.com>
2025-04-11 15:12:12 +0100

    i change something in my quiz

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function Button({handleAdd}) {
-
-    return(
-        <button onClick={handleAdd} className='block bg-white p-3 rounded w-fll '>add question</button>
-    )
+function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={onClick}
+      className="block bg-blue-600 text-white p-3 rounded w-full hover:bg-blue-700 transition"
+    >
+      {children}
+    </button>
+  );
 }
+
 export default Button;
